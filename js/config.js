b7a22bf5cd551db9f6948578339a25f942adb9eb
Owiti95 <132479149+Owiti95@users.noreply.github.com>
2025-11-28 20:32:55 +0300

    Update API invokeUrl in config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,8 @@ window._config = {
         region: 'eu-north-1'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://3a3mna91qj.execute-api.eu-north-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
